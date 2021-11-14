c7f64bcd99a5587b6f9ca051abb8795cb2e1e793
Vynstan <HappyM0f0@icloud.com>
2021-11-14 11:49:36 -0600

    assignment updates

--- plots.js
+++ plots.js
@@ -1,37 +1,93 @@
-// Sort the data array using the greekSearchResults value
-data.sort(function(a, b) {
-  return parseFloat(b.greekSearchResults) - parseFloat(a.greekSearchResults);
+console.log("Hello");
+
+// load local server
+d3.json("samples.json").then(function(data){
+    console.log(data);
 });
 
-// Slice the first 10 objects for plotting
-data = data.slice(0, 10);
+// extract only the wfreq
+d3.json("samples.json").then(function(data){
+    wfreq = data.metadata.map(person => person.wfreq);
+    console.log(wfreq);
+});
 
-// Reverse the array due to Plotly's defaults
-data = data.reverse();
+// sort the wfreq array in descending order
+d3.json("samples.json").then(function(data){
+    wfreq = data.metadata.map(person =>
+person.wfreq).sort((a,b) => b - a);
+    console.log(wfreq);
+});
 
-// Trace1 for the Greek Data
-var trace1 = {
-  x: data.map(row => row.greekSearchResults),
-  y: data.map(row => row.greekName),
-  text: data.map(row => row.greekName),
-  name: "Greek",
-  type: "bar",
-  orientation: "h"
+//  delete null values from the sorted wfreq array
+d3.json("samples.json").then(function(data){
+    wfreq = data.metadata.map(person =>
+person.wfreq).sort((a,b) => b - a);
+    filteredWfreq = wfreq.filter(element => element !=
+null);
+    console.log(filteredWfreq);
+});
+
+// object.entries()
+
+const researcher1 = {
+    name: 'Roza',
+    age: 34,
+    hobby: 'Hiking'
 };
 
-// data
-var data = [trace1];
+console.log(Object.entries(researcher1));
 
-// Apply the group bar mode to the layout
-var layout = {
-  title: "Greek gods search results",
-  margin: {
-    l: 100,
-    r: 100,
-    t: 100,
-    b: 100
-  }
-};
+//skill drill Use Object.entries()and forEach() to print all the metadata of the first person in the samples.json() dataset (ID 940)
+d3.json("samples.json").then(function(data){
+    firstPerson = data.metadata[0];
+    console.log(firstPerson);
+});
 
-// Render the plot to the div tag with id "plot"
-Plotly.newPlot("plot", data, layout);
+
+d3.json("samples.json").then(function(data){
+    firstPerson = data.metadata[0];
+    Object.entries(firstPerson).forEach(([key, value]) =>
+      {console.log(key + ': ' + value);});
+});
+
+
+// let d1 = researcher1.prototype.forEach(([first, second]) => console.log(first
+//     + ": " + second));
+
+// console.log(d1)
+
+
+function init() {
+    data = [{
+      x: [1, 2, 3, 4, 5],
+      y: [1, 2, 4, 8, 16] }];
+    Plotly.newPlot("plot", data);
+  };
+  
+  d3.selectAll("#dropdownMenu").on("change", updatePlotly);
+  function updatePlotly() {
+    var dropdownMenu = d3.select("#dropdownMenu");
+    var dataset = dropdownMenu.property("value");
+  
+    var xData = [1, 2, 3, 4, 5];
+    var yData = [];
+  
+    if (dataset === 'dataset1') {
+      yData = [1, 2, 4, 8, 16];
+    };
+  
+    if (dataset === 'dataset2') {
+      yData = [1, 10, 100, 1000, 10000];
+    };
+  
+    var trace = {
+      x: [xData],
+      y: [yData],
+    };
+    Plotly.restyle("plot", trace);
+  };
+  
+  init();
+
+
+  
